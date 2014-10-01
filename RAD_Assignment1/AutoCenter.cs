71aa57e2f3722b01657848e857a59899b866ed49
parkjonghwa75 <jaypark.1120@gmail.com>
2014-10-01 15:13:15 -0400

    form designing

--- RAD_Assignment1/AutoCenter.cs
+++ RAD_Assignment1/AutoCenter.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RAD_Assignment1
 {
-    public partial class Form1 : Form
+    public partial class AutoCenter : Form
     {
-        public Form1()
+        public AutoCenter()
         {
             InitializeComponent();
         }
